caa563178463d6b92c8c6f3751a8e04d0f60c2c4
Anderson Correa <andersonfpcorrea@gmail.com>
2023-03-17 16:33:57 -0300

    feat: create basic structure of home page

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -6,8 +6,22 @@ describe("Unit tests for Home component", () => {
   test("It checks if all components are rendered accordigly", () => {
     const testId = "1";
     const view = render(<Home testId={testId} />);
+
     const home = screen.getByTestId(`home-${testId}`);
+    const title = screen.getByTestId(`home-title-${testId}`);
+    const name = screen.getByTestId(`home-input-name-${testId}`);
+    const linkedin = screen.getByTestId(`home-input-linkedin-${testId}`);
+    const github = screen.getByTestId(`home-input-github-${testId}`);
+    const button = screen.getByTestId(`home-button-${testId}`);
+
     expect(home).toBeInTheDocument();
+    expect(title).toHaveTextContent("QR Code Image Generator");
+    expect(name).toHaveAttribute("type", "text");
+    expect(linkedin).toHaveAttribute("type", "text");
+    expect(github).toHaveAttribute("type", "text");
+    expect(button).toHaveAttribute("type", "button");
+    expect(button).toHaveTextContent("Generate Image");
+
     view.unmount();
   });
 });
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,41 @@
+import { Button, Input, Space, Typography } from "antd";
+
 interface IHomeProps {
   testId?: string;
 }
 
 export function Home({ testId = "" }: IHomeProps): JSX.Element {
-  return <div data-testid={`home-${testId}`}>Home</div>;
+  return (
+    <Space direction="vertical" size="large" data-testid={`home-${testId}`}>
+      <Typography.Title data-testid={`home-title-${testId}`}>
+        QR Code Image Generator
+      </Typography.Title>
+      <Space direction="vertical" size="middle">
+        <Input
+          addonBefore="Name"
+          placeholder="John"
+          size="large"
+          type="text"
+          data-testid={`home-input-name-${testId}`}
+        />
+        <Input
+          addonBefore="Linkedin URL"
+          placeholder="https://www.linkedin.com/john"
+          size="large"
+          type="text"
+          data-testid={`home-input-linkedin-${testId}`}
+        />
+        <Input
+          addonBefore="Github URL"
+          placeholder="https://www.github.com/john"
+          size="large"
+          type="text"
+          data-testid={`home-input-github-${testId}`}
+        />
+      </Space>
+      <Button size="large" type="primary" data-testid={`home-button-${testId}`}>
+        Generate Image
+      </Button>
+    </Space>
+  );
 }
