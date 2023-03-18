f77c23c2fe2f83b38d0767128bbb03c10946b333
Anderson Correa <andersonfpcorrea@gmail.com>
2023-03-17 21:19:26 -0300

    refactor: refactor use-qr-code custom hook

--- src/hooks/useQrCode.ts
+++ src/hooks/useQrCode.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { createObjFromFormEntries, generateUrlFromFormData } from "../utils";
 
 interface IUseQrCodeReturn {
@@ -13,25 +13,29 @@ export const useQrCode = (): IUseQrCodeReturn => {
   const [value, setValue] = useState<IUseQrCodeReturn["value"]>(undefined);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleModalOk = (): void => {
+  const handleModalOk = useCallback((): void => {
     setIsModalOpen(false);
-  };
-  const handleModalCancel = (): void => {
-    setIsModalOpen(false);
-  };
+  }, [setIsModalOpen]);
 
-  const showModal = (): void => {
+  const handleModalCancel = useCallback((): void => {
+    setIsModalOpen(false);
+  }, [setIsModalOpen]);
+
+  const showModal = useCallback((): void => {
     setIsModalOpen(true);
-  };
+  }, [setIsModalOpen]);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    const formData = createObjFromFormEntries<"qrcode">(e.currentTarget);
-    const url = generateUrlFromFormData(formData);
-    console.log("URL:", url);
-    setValue(url);
-    showModal();
-  };
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>): void => {
+      e.preventDefault();
+      const formData = createObjFromFormEntries<"qrcode">(e.currentTarget);
+      const url = generateUrlFromFormData(formData);
+      console.log("URL:", url);
+      setValue(url);
+      showModal();
+    },
+    [setValue, showModal]
+  );
 
   return {
     value,
